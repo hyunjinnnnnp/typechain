3915f0c379e9dc8cc6e3331e624ac4b7afe07e0c
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-06-23 21:58:09 +0900

    object & interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  // : 함수가 어떤 값을 반환할 것인지 명시해준다
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+//interface는 JS에서 작동 안함
+
+const person = {
+  name: "nico",
+  age: 22,
+  gender: "male",
 };
-console.log(sayHi("Nico", 555, "male"));
+const sayHi = (person: Human): string => {
+  // : 함수가 어떤 값을 반환할 것인지 명시해준다
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+console.log(sayHi(person));
 
 //이 파일이 모듈이 된다는 걸 알 수 있도록 export해준다
 export {};
