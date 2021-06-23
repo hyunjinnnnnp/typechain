3674cc70422679de41a228d16543325bc21a92c1
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-06-23 21:16:30 +0900

    first steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("Hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Nico", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-alert("Hello");
+const name = "Nico",
+  age = 24,
+  gender = "male";
+
+const sayHi = (name, age, gender) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
+
+//이 파일이 모듈이 된다는 걸 알 수 있도록 export해준다
+export {};
